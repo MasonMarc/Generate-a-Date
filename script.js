65e7722ce6292b88d6e84a888c71c497d445b89c
MasonMarc <masonmarcantel@gmail.com>
2022-12-12 12:11:33 -0500

    got movie api to work to make random movie

--- script.js
+++ script.js
@@ -36,26 +36,29 @@ var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + liquo
             alert('Unable to connect');
         });
 };
-// var getMovie = function () {
-// var apiUrl = 'http://www.omdbapi.com/?y=1989&apikey=5523dad2';
-//     fetch(apiUrl)
-//         .then(function (response) {
-//             if (response.ok) {
-//                 response.json().then(function (data) {
-//                     console.log(data);
-//                 });
-//             } else {
-//                 alert('Error: ' + response.statusText);
-//             }
-//         })
-//         .catch(function (error) {
-//             alert('Unable to connect');
-//         });
-// };
+var getMovie = function () {
+    var rand1 = Math.floor(1000000 + Math.random() * 1000000);
+    var rand = rand1.toString();
+var apiUrl = 'http://www.omdbapi.com/?i=tt' + rand + '&apikey=5523dad2';
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect');
+        });
+};
 
 $('#init').click(function (event) { 
     event.preventDefault();
     getMeal();
     getDrink();
+    getMovie();
     
 });
