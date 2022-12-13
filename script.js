397fd0a663270bd0e9cf78ee7f6e2baffa611952
SamsonKnightz <113211927+SamsonKnightz@users.noreply.github.com>
2022-12-13 10:17:07 -0500

    added comments and moved container in html

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var getMeal = function () {
     var ingredient = $('#ingredient').val();
-var apiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=' + ingredient
+    var apiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=' + ingredient
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
@@ -19,7 +19,7 @@ var apiUrl = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=' + ingredien
 };
 var getDrink = function () {
     var liquor = $('#liquor option:selected').val();
-var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + liquor;
+    var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + liquor;
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
@@ -40,7 +40,7 @@ var apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + liquo
 var getMovie = function () {
     var rand1 = Math.floor(10000 + Math.random() * 10000);
     var rand = rand1.toString();
-var apiUrl = 'http://www.omdbapi.com/?i=tt00' + rand + '&apikey=5523dad2';
+    var apiUrl = 'http://www.omdbapi.com/?i=tt00' + rand + '&apikey=5523dad2';
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
@@ -56,35 +56,35 @@ var apiUrl = 'http://www.omdbapi.com/?i=tt00' + rand + '&apikey=5523dad2';
         });
 };
 var displayMeal = function (meals) {
-        var randMeal = Math.floor(meals.length * Math.random());
-        var mealName = meals[randMeal].strMeal;
-        console.log(mealName);
-        console.log(randMeal);
-        $('#meal-title').text(mealName);
-        var mealpicurl = meals[randMeal].strMealThumb
-        $("#mealPic").attr("src", mealpicurl)
-        var meallink = "https://www.google.com/search?q=" + mealName
-        $("#meallink").attr("href", meallink)
-        
+    var randMeal = Math.floor(meals.length * Math.random());
+    var mealName = meals[randMeal].strMeal;
+    console.log(mealName);
+    console.log(randMeal);
+    $('#meal-title').text(mealName);
+    var mealpicurl = meals[randMeal].strMealThumb
+    $("#mealPic").attr("src", mealpicurl)
+    var meallink = "https://www.google.com/search?q=" + mealName
+    $("#meallink").attr("href", meallink)
 
-     }
-     var displayDrink = function (drinks) {
-        var randDrink = Math.floor(drinks.length * Math.random());
-        var drinksName = drinks[randDrink].strDrink;
-        console.log("test")
-        console.log(randDrink);
-        $('#drink-title').text(drinksName);
-        var drinkspicurl = drinks[randDrink].strDrinkThumb
-        $("#drinkPic").attr("src", drinkspicurl)
-        var drinkslink = "https://www.google.com/search?q=" + drinksName
-        $("#drinklink").attr("href", drinkslink)
-        
 
-     }
-$('#init').click(function (event) { 
+}
+var displayDrink = function (drinks) {
+    var randDrink = Math.floor(drinks.length * Math.random());
+    var drinksName = drinks[randDrink].strDrink;
+    console.log("test")
+    console.log(randDrink);
+    $('#drink-title').text(drinksName);
+    var drinkspicurl = drinks[randDrink].strDrinkThumb
+    $("#drinkPic").attr("src", drinkspicurl)
+    var drinkslink = "https://www.google.com/search?q=" + drinksName
+    $("#drinklink").attr("href", drinkslink)
+
+
+}
+$('#init').click(function (event) {
     event.preventDefault();
     getMeal();
     getDrink();
     getMovie();
-    
+
 });
